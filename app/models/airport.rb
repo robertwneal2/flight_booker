b9c0e47a5b08a210d70afced8ad0080bcc60b9b5
Robert Neal <robertwneal2@gmail.com>
2022-11-11 07:07:21 -0800

    Add model validations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,6 @@
 class Airport < ApplicationRecord
+  validates: :code, presence: true
+
   has_many :departures, class_name: "Flight", foreign_key: :departure_id
   has_many :arrivals, class_name: "Flight", foreign_key: :arrival_id
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
+  validates: :start, :duration, presence: true
+
   belongs_to :departure, class_name: "Airport"
   belongs_to :arrival, class_name: "Airport"
 end
