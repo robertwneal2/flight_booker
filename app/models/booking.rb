5c44cac844c6b5a9c1cd5c715370a07489b90d1c
Robert Neal <robertwneal2@gmail.com>
2022-11-23 09:33:58 -0800

    Remove unnecessary source in has many to has many model associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passenger_bookings, foreign_key: :booking_id
-  has_many :passengers, through: :passenger_bookings, source: :passenger, dependent: :destroy
+  has_many :passengers, through: :passenger_bookings, dependent: :destroy
   accepts_nested_attributes_for :passengers
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,4 @@
 class Passenger < ApplicationRecord
   has_many :passenger_bookings, foreign_key: :passenger_id
-  has_many :bookings, through: :passenger_bookings, source: :booking, dependent: :destroy
+  has_many :bookings, through: :passenger_bookings, dependent: :destroy
 end
